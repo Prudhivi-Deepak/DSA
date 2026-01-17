51f3f17cee88771ff958af44f443952622df6c51
pdeepak <prudhivi.deepak@amadeus.com>
2026-01-17 19:39:44 +0530

    Leetcode Daily DSA

---
+++ Daily_Leetcode/Math_or_bruteforce/3047_Find_the_Largest_Area_of_Square_Inside_Two_Rectangles.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    long long largestSquareArea(vector<vector<int>>& bottomLeft,
+                                vector<vector<int>>& topRight) {
+        int n = bottomLeft.size();
+        int maxside = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                int width = min(topRight[i][0], topRight[j][0]) - max(bottomLeft[i][0], bottomLeft[j][0]);
+                int height = min(topRight[i][1], topRight[j][1]) - max(bottomLeft[i][1], bottomLeft[j][1]);
+                maxside = max(maxside, min(width,height));
+            }
+        }
+        return 1LL*maxside*maxside;
+    }
+};
